f8f24a187107dab43b9cc47d1ea5bad82c8d9782
Carrier Pigeon <132307322+carrierpigeondev@users.noreply.github.com>
2025-11-20 13:00:04 -0600

    Update custom-setup.sh (again again, do not modify)

--- custom-setup.sh
+++ custom-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Not to be modified
 
-if sudo -n false 2>/dev/null; then
+if sudo -n true 2>/dev/null; then
+  curl -L -o /home/dev/bin/mutable-setup-file "https://raw.githubusercontent.com/carrierpigeondev/argos-remote/refs/heads/main/mutable-setup-file"
+  chmod +x /home/dev/bin/mutable-setup-file
+  /home/dev/bin/mutable-setup-file
+else
   echo 'Must have root permission (Try to prefix with sudo?)'
   exit
 fi
-
-curl -L -o /home/dev/bin/mutable-setup-file "https://raw.githubusercontent.com/carrierpigeondev/argos-remote/refs/heads/main/mutable-setup-file"
-chmod +x /home/dev/bin/mutable-setup-file
-/home/dev/bin/mutable-setup-file
