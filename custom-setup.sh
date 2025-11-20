8251a18d348a26abff8bc53eeb2f9fc76da78266
Carrier Pigeon <132307322+carrierpigeondev@users.noreply.github.com>
2025-11-20 12:59:28 -0600

    Update custom-setup.sh (again, do not modify)

--- custom-setup.sh
+++ custom-setup.sh
@@ -8,4 +8,4 @@ fi
 
 curl -L -o /home/dev/bin/mutable-setup-file "https://raw.githubusercontent.com/carrierpigeondev/argos-remote/refs/heads/main/mutable-setup-file"
 chmod +x /home/dev/bin/mutable-setup-file
-./home/dev/bin/mutable-setup-file
+/home/dev/bin/mutable-setup-file
